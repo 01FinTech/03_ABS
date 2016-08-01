3c3e4d088c593732320c9a6151dac4061682de77
HaifengHaifeng <xhf@jomihos-Mac-mini.local>
2016-08-01 15:16:43 +0800

    读取 excel
    如何计算日期之间的差距

---
+++ ABS.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+## ABS
+## Haifeng XU
+## QQ: 78112407
+## Email: hfxu@wxtrust.com
+
+import numpy as np
+import pandas as pd
+import datetime
+
+df = pd.read_excel('abs.xlsx')
+print ( df )
+
+df_date = df.iloc[:,4:]
+print ( df_date )
+
+tmp1 = ( df_date.iloc[0,1] - df_date.iloc[0,0] ).days * 100000 - 0.0
+tmp1 = format( tmp1, ',')
+print ( tmp1 )
