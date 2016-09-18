16e9423757e3740adebabf654cc45683e4374f29
HaifengHaifeng <haifengxu9@163.com>
2016-09-18 18:27:08 +0800

    增加了分类统计
    按省、市、类型进行统计

--- ABS.py
+++ ABS.py
@@ -5,7 +5,7 @@
 ## Dr. Haifeng XU
 ## QQ: 78112407
 ## Email: hfxu@wxtrust.com
-## modified on Mon, Aug 15, 2016
+## modified on Sun, Sep 18, 2016
 ## ------------------------- ##
 
 import numpy as np
@@ -14,33 +14,37 @@ import datetime
 
 ## 封包日
 ## 注意: 这里与后面的列名称保持一致
-_tmp_date = '2016-08-30'
+_tmp_date = '2016-10-19'
 _project_fixed = pd.DataFrame ( { 'Date' : pd.Timestamp( _tmp_date) }, \
-								index = {'fixDate'} )
+				index = {'fixDate'} )
 
-## --------------------------------
-## todo
-## 如何通过列名得到这些数字?
-## --------------------------------
+'''
+----------------------------
+todo: 如何通过列名得到这些数字?
+----------------------------
+'''
 
 ## excel 中, 项目名称所在列
-_project_name = 0
+_project_name = 1
 
 ## excel 中, 项目本金所在列
-_project_principle = 8
+_project_principle = 3
 
 ## excel 中, 项目利率所在列
-_project_interest = 5
+_project_interest = 7
 
 ## excel 中, 派息日从该列开始
-_interest_begin = 19
+_interest_begin = 21
 
 
-df = pd.read_excel ( 'ABSINPUT.xlsx' )
-df = df.rename ( columns = { u'产品名称' : 'project' } )
-df = df.rename ( columns = { u'派息日'  : 'interest_day' } )
-
-print ( df )
+df = pd.read_excel( 'ABSINPUT.xlsx' )
+df = df.rename( columns = { u'产品名称'         : '_col_project'        } )
+df = df.rename( columns = { u'派息日期'         : '_col_interest_day'   } )
+df = df.rename( columns = { u'存续金额（元）'   : '_col_principle'      } )
+df = df.rename( columns = { u'融资方省'         : '_col_province'       } )
+df = df.rename( columns = { u'融资方市/县'      : '_col_city'           } )
+df = df.rename( columns = { u'类型'             : '_col_industry'       } )
+df = df.rename( columns = { u'入池'             : '_col_include'        } )
 
 ## df_cashflow 是最终输出的现金流量表
 df_cashflow = pd.DataFrame()
@@ -48,60 +52,50 @@ df_cashflow = pd.DataFrame()
 ## --------------------------------------------------------- ##
 ## begin of for
 ## --------------------------------------------------------- ##
-for i in range (0, len( df )) :
+for i in range( len( df ) ) :
 
 	## 创建一个空的临时 df
 	df_tmp = pd.DataFrame()
 
 	## 将该行的派息日期加入上面的临时 df
-	df_tmp = pd.DataFrame( df.iloc[ i:(i+1), _interest_begin: ]).T
+	df_tmp = pd.DataFrame( df.iloc[ i:(i+1) , _interest_begin: ] ).T
 
 	## 为了与后面保持统一, 日期列名都是 1
-	df_tmp = df_tmp.rename ( columns = { i : 'Date' } )
+	df_tmp = df_tmp.rename( columns = { i : 'Date' } )
 
 	## 删除该列中含有空日期的行
-	df_tmp = df_tmp.dropna ()
+	df_tmp = df_tmp.dropna()
 
-	## -------------------
-	## todo
-	## 这里需要判断一下, 封包日是否在派息日
-	## --------------------
-
-	## --------------------
+	'''
+        -------------------------------------
+        Todo: 这里需要判断一下, 封包日是否在派息日
+        -------------------------------------
+	'''
 
 	## 插入封包日
 	df_tmp = df_tmp.append( _project_fixed )
 	## 根据日期排序
-	df_tmp = df_tmp.sort_values( by = 'Date' )
+	df_tmp = df_tmp.sort_values( by = 'Date' , ascending = [True] )
 
 	## 插入项目名称
-	df_tmp[ 'Project' ] = df.iloc[ i ][ _project_name ]
+	df_tmp[ 'Project' ]   = df.iloc[ i ][ _project_name ]
 	## 插入项目利率
-	df_tmp[ 'Rate' ] = df.iloc[ i ][ _project_interest ]
+	df_tmp[ 'Rate' ]      = df.iloc[ i ][ _project_interest ]
 	## 插入项目本金
 	df_tmp[ 'Principle' ] = df.iloc[ i ][ _project_principle ]
+
 	## 插入相差天数
 	df_tmp [ 'Day' ] = 0
-
 	## 计算每个计息周期
 	for i in range ( 1, len ( df_tmp ) ) :
 		df_tmp[ 'Day' ][ i ] = ( df_tmp['Date'][i] - df_tmp['Date'][i-1]  ).days
-		## --------------------------------
-		## 测试
-		## --------------------------------
-#		print ( i )
-#		print (  df_tmp['Date'][i] )
-#		print ( df_tmp['Date'][i-1])
-#		print ( df_tmp[ 'Day' ][ i ]  )
-		## --------------------------------
-
 
 	## --------------------------------
 	## todo
 	##  这里的 365 指年化天数, 应该根据具体项目修改
 	## --------------------------------
 	## 计算每个付息日应收利息
-	df_tmp[ 'Interest' ] =  df_tmp[ 'Principle' ] * df_tmp[ 'Rate' ] * df_tmp[ 'Day' ] / 365
+	df_tmp[ 'Interest' ] = df_tmp[ 'Principle' ] * df_tmp[ 'Rate' ] * df_tmp[ 'Day' ] / 365
 
 	## 最后一期才归还本金, 其他部分还是0
 	for i in range ( 0 , len ( df_tmp ) - 1 ) :
@@ -110,6 +104,7 @@ for i in range (0, len( df )) :
 	## 流入现金流合计
 	df_tmp[ 'Sum' ] = df_tmp[ 'Principle' ] + df_tmp[ 'Interest' ]
 
+	'''
 	## --------------------------------
 	## todo
 	## 如何正确显示千位符
@@ -119,7 +114,7 @@ for i in range (0, len( df )) :
 	## 重新命名 Index
 	## 如何验证计算正确性?
 	## --------------------------------
-	##	df_tmp[ 'Sum' ] = format ( df_tmp [ 'Sum' ] , ',' )
+	'''
 
 	## 删除天数所在列
 	del df_tmp[ 'Day' ]
@@ -143,14 +138,49 @@ df_cashflow.to_excel ( 'ABSoutput.xlsx' )
 ## 按日期排序输出
 ## 仅仅输出封包日之后的现金流
 df_cashflow[ df_cashflow['Date'] > _tmp_date ] \
-	.sort( columns = ['Date'], ascending = [True] ) \
-	.to_excel( 'ABSoutputByDate.xlsx')
+                .sort_values( by = 'Date' , ascending = [True] ) \
+	        	.to_excel( 'ABSoutputByDate.xlsx' )
+
+## ------------------------------------------------------------------------ ##
+## 针对每一个属性进行分类统计
+## ------------------------------------------------------------------------ ##
+def _my_pivot_table( df , _col ) :
+
+	df_new = pd.DataFrame()
+	df_new[ u'本金' ]    	= df.groupby( _col )[ '_col_principle' ].sum()
+	df_new[ u'本金占比' ]  = df_new[ u'本金' ] / df_new[ u'本金' ].sum()
+	df_new[ u'个数']       = df.groupby( _col ).size()
+	df_new[ u'个数占比' ]  = df_new[ u'个数' ] / df_new[ u'个数' ].sum()
+	df_new = df_new.sort_values( u'本金', ascending = False )
+
+	df_tmp = pd.DataFrame( df_new.sum(), \
+				columns = { u'合计'} )
+
+	df_new = df_new.append( df_tmp.T )
+	print 'I found the df_new'
+	print df_new
+	return df_new
+## ------------------------------------------------------------------------ ##
 
 
 
+'''
+df_province = _my_pivot_table( df, '_col_province' )
+df_city     = _my_pivot_table( df, '_col_city' )
+df_industry = _my_pivot_table( df, '_col_industry' )
+'''
 
+
+df_final = pd.DataFrame()
+df_final = df_final.append( _my_pivot_table( df, '_col_province' ) )
+df_final = df_final.append( _my_pivot_table( df, '_col_city' ) ) 
+df_final = df_final.append( _my_pivot_table( df, '_col_industry' ) )
+
+print df_final
+df_final.to_excel( 'pivot.xlsx' )
 
 
 
 
 ## -------------------------------
+
